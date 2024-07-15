6f7e44742d454fe6503166486b3702c998deecff
NegenshinCorporated <dzubenkoaleksandr4@gmail.com>
2024-07-15 23:25:37 +0300

    Создал Таймер

---
+++ Space/Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Threading;
+
+public class Timer : MonoBehaviour
+{
+    public float timer;
+    private float startTime = 40f;
+    public TextMeshProUGUI timerText;
+    private int t;
+    private void Start()
+    {
+        timerText.text = "Timer:" + timer.ToString();
+    }
+    void Update()
+    {
+        if (timer < startTime)
+        {
+            timer += Time.deltaTime;
+            float remainingTime = startTime - timer;
+            int intrmainingTime = (int)remainingTime;
+            t = intrmainingTime;
+            timerText.text = "Timer:" + t.ToString();
+        }
+    }
+}
